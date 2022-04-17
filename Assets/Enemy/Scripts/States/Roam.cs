0e35abd90f878c3c2608050334356a8f132f4b68
lcscout <lcscoutinho@outlook.com>
2022-04-17 11:39:28 -0300

    feat(AI): make enemy AI wait after each roam it does

--- Assets/Enemy/Scripts/States/Roam.cs
+++ Assets/Enemy/Scripts/States/Roam.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Threading.Tasks;
+using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
 
@@ -7,8 +8,11 @@ public class Roam : IState {
 	private NavMeshAgent _navMeshAgent;
 
 	private float _initialSpeed;
-	private const float ROAM_SPEED = 2F;
-	private const float ROAM_DISTANCE = 5F;
+	private bool _canRoam = true;
+
+	private readonly float[] ROAM_SPEED_MIN_MAX = { 2f, 6f };
+	private readonly float[] ROAM_DISTANCE_MIN_MAX = { 10f, 20f };
+	private readonly int[] ROAM_WAIT_TIME_MIN_MAX = { 2, 5 };
 
 	public Roam(EnemyController enemy, NavMeshAgent navMeshAgent) {
 		_enemy = enemy;
@@ -18,13 +22,20 @@ public class Roam : IState {
 	public void OnEnter() {
 		_navMeshAgent.enabled = true;
 		_initialSpeed = _navMeshAgent.speed;
-		_navMeshAgent.speed = ROAM_SPEED;
+		_navMeshAgent.speed = Random.Range(ROAM_SPEED_MIN_MAX[0], ROAM_SPEED_MIN_MAX[1]);
+	}
+
+	public void OnExit() {
+		_navMeshAgent.speed = _initialSpeed;
+		_navMeshAgent.enabled = false;
 	}
 
 	public void Tick() {
-		if (_navMeshAgent.remainingDistance < 1f) {
+		if (_navMeshAgent.pathStatus == NavMeshPathStatus.PathComplete && _navMeshAgent.remainingDistance < 1 && _canRoam) {
 			Vector3 away = GetRandomPoint();
 			_navMeshAgent.SetDestination(away);
+
+			WaitToRoamAgain();
 		}
 	}
 
@@ -32,7 +43,7 @@ public class Roam : IState {
 		Vector3 direction = Random.insideUnitSphere;
 		direction.y = 0;
 
-		Vector3 endPoint = _enemy.transform.position + (direction * ROAM_DISTANCE);
+		Vector3 endPoint = _enemy.transform.position + (direction * Random.Range(ROAM_DISTANCE_MIN_MAX[0], ROAM_DISTANCE_MIN_MAX[1]));
 		if (NavMesh.SamplePosition(endPoint, out NavMeshHit hit, 10f, NavMesh.AllAreas)) {
 			return hit.position;
 		}
@@ -40,8 +51,11 @@ public class Roam : IState {
 		return _enemy.transform.position;
 	}
 
-	public void OnExit() {
-		_navMeshAgent.speed = _initialSpeed;
-		_navMeshAgent.enabled = false;
+	private async void WaitToRoamAgain() {
+		_canRoam = false;
+
+		await Task.Delay(Random.Range(ROAM_WAIT_TIME_MIN_MAX[0], ROAM_WAIT_TIME_MIN_MAX[1]) * 1000);
+
+		_canRoam = true;
 	}
 }
